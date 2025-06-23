3e8494d683f46c7096b7734e197e91405acf4e5d
Chethaka Sathmal <csathmal24@gmail.com>
2025-06-23 18:58:32 +0530

    fixed a typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1>Chetaka Sathmal</h1>
+        <h1>Chethaka Sathmal</h1>
         <h2>Software developer based on Sri Lanka</h2>
       </div>
     </main>
